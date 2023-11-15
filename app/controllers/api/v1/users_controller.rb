f8e7d35e28c097b8a91dd29093f914adf5205488
Daniel Lessenden <dless27@gmail.com>
2023-11-14 18:22:14 -0700

    Update the users controller to utilize the serial and delete oauth usage

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -18,15 +18,15 @@ class Api::V1::UsersController < ApplicationController
   end
   
   def create
-    user = User.from_onmiauth(user_params)
-    return nil if !user.save
-    render json: UserSerializer.new(user)
+    user = User.new(user_params)
+    render json: UserSerializer.new(user) if user.save
   end
 
   def update
     return nil if !User.exist?(params[:id])
     render json: UserSerializer.new(User.update(params[:id], user_params))
   end
+  
 
   def destroy
     return nil if !User.exist?(params[:id])
@@ -36,13 +36,6 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def user_params
-    if params[:update_user]
-      params.require(:update_user).permit(:email)
-    else
-      a = params.permit(:uid, :provider)
-      b = params.require(:credentials).permit(:token, :refresh_token)
-      c = params.require(:info).permit(:email)
-      a.merge(b).merge(c)
-    end
-  end
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end 
 end
