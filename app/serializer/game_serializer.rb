7ee1f193f0af4ea33f7fd1f47a2cc46f5a20ca1a
Daniel Lessenden <dless27@gmail.com>
2023-11-30 21:53:37 -0700

    Edit game serializer

--- app/serializer/game_serializer.rb
+++ app/serializer/game_serializer.rb
@@ -1,6 +1,6 @@
 class GameSerializer
     include FastJsonapi::ObjectSerializer
-    attributes :id, :result, :total_guesses, :target_word
+    attributes :id, :result, :total_guesses, :target_word, :game_board
 
     belongs_to :game_history
     # belongs_to :user
